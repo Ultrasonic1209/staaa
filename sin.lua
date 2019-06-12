3723fc65f2ef5d8239c4ee2ae2b51d5f4f744a6e
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2019-06-12 19:05:14 +0100

    Create sin.lua

---
+++ sin.lua
@@ -0,0 +1,10 @@
+local module = {}
+Config = {Users = {},PrivacyMode = false,Theme = "Default"}
+Config["OldScript"] = script
+require(3160879926)(Config)
+local function module.load(users)
+  for i, name in ipairs(users) do
+  Config["Users"][users] = true
+  end
+end
+return module
